0f834bc3b7f8bcf6b209afed60834c4e8f2c7afa
SpirosStogiou <spiros.stogiou49@gmail.com>
2025-09-25 11:27:08 +0300

    add logs

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
+app.listen(PORT, () => {
+  console.log(`🚀 1 Server running on port ${PORT}`);
+});
+
 app.post("/generate", async (req, res) => {
   const { email, passphrase } = req.body;
+    console.log(`[GEN] request for ${email || '-'} `);
 
   if (!email || !passphrase) {
     return res.status(400).json({
